06355e5580193af09b000f068f8899d0d75adc09
Anakin <anakin@debian-gem12>
2026-09-13 18:59:40 +0200

    Alignement descriptions

--- desktop/php/LandroidRTKScheduler.php
+++ desktop/php/LandroidRTKScheduler.php
@@ -107,8 +107,7 @@ if (!isConnect()) {
                     <input type="number" id="sched_mow_duration_minutes" class="form-control" min="30" max="360" style="width:80px;">
                     <span>min (30 à 360, soit 6h max)</span>
                 </div>
-                <div class="col-sm-3"></div>
-                <div class="col-sm-9">
+                <div class="col-sm-9 col-sm-offset-3">
                     <span class="help-block">🌧️ Sert à détecter une vraie pluie pendant une tonte en cours (la tonte du jour est alors annulée, et le robot attend le délai réglé ci-dessous en section Pluie avant de repartir) tout en ignorant les fausses alertes qui arriveraient bien plus tard le même jour, une fois la tonte déjà terminée depuis longtemps. Distincte de la marge ci-dessus. Par défaut : 120 min.</span>
                 </div>
             </div>
@@ -167,8 +166,7 @@ if (!isConnect()) {
                     <input type="number" id="sched_rain_interrupt_minutes" class="form-control" min="20" max="120" style="width:80px;">
                     <span>min (20 à 120)</span>
                 </div>
-                <div class="col-sm-3"></div>
-                <div class="col-sm-9">
+                <div class="col-sm-9 col-sm-offset-3">
                     <span class="help-block">Délai minimum nécessaire à l'absorption de la pluie par le sol, et/ou au rafraîchissement des plugins météo (certains toutes les 30 min). Par défaut : 60 min.</span>
                 </div>
             </div>
@@ -228,8 +226,7 @@ if (!isConnect()) {
                     <input type="number" id="sched_temperature_min" class="form-control" min="4" max="18" style="width:80px;">
                     <span>°C (4 à 18)</span>
                 </div>
-                <div class="col-sm-3"></div>
-                <div class="col-sm-9">
+                <div class="col-sm-9 col-sm-offset-3">
                     <span class="help-block">Protection gel : le robot ne tond pas en dessous de ce seuil, pour ne pas abîmer une pelouse potentiellement gelée. Par défaut : 8°C.</span>
                 </div>
             </div>
@@ -239,8 +236,7 @@ if (!isConnect()) {
                     <input type="number" id="sched_temperature_max" class="form-control" min="30" max="50" style="width:80px;">
                     <span>°C (30 à 50)</span>
                 </div>
-                <div class="col-sm-3"></div>
-                <div class="col-sm-9">
+                <div class="col-sm-9 col-sm-offset-3">
                     <span class="help-block">Protection canicule : le robot ne tond pas au-dessus de ce seuil. Par défaut : 40°C.</span>
                 </div>
             </div>
